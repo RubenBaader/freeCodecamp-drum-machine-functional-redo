3e0bc27ce94e2c7ed3f2c1ab076914c35e278c9b
RubenBaader <rubenbaader@gmail.com>
2023-02-27 15:28:02 +0100

    config complete

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import React from "react";
 import reactDom from "react-dom";
 import App from "./src/App"
 
+console.log("I'm alive!", document.getElementById("root"));
 reactDom.render(<App />, document.getElementById("root"));
