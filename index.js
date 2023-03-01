75e737dd683ad11176e5e05ecdd07061b8ec8f3e
RubenBaader <rubenbaader@gmail.com>
2023-03-01 14:39:01 +0100

    updated to use createRoot rather than reactDom.render

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import React from "react";
-import reactDom from "react-dom";
+// import reactDom from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./src/App"
 
-reactDom.render(<App />, document.getElementById("root"));
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />)
