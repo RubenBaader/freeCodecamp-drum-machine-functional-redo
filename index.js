b75aef216dab8a8da1c1131039a44e3b7f637bea
RubenBaader <rubenbaader@gmail.com>
2023-03-02 19:55:47 +0100

    added basic styling

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from "react";
 // import reactDom from "react-dom";
 import { createRoot } from 'react-dom/client';
-import App from "./src/App"
-
+import App from "./src/App";
+// import css from "./src/stylesheet.css";
+// import css from "/public/stylesheet.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "../public/stylesheet.css";
 
 // On button press : Play audio from child audio tag
 // Child has an id to get element by
@@ -38,92 +39,94 @@ const App = () => {
             <div id="display">
                 <h2>{display}</h2>
             </div>
-            <button className="drum-pad" id="Heater-one" onClick={() => handleAudio("Q")}>
-                Q
-                <audio  
-                    src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-                    className="clip" 
-                    id="Q"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
-            <button className="drum-pad" id="Heater-two" onClick={() => handleAudio("W")}>
-                W
-                <audio 
-                    src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" 
-                    className="clip" 
-                    id="W"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
-            <button className="drum-pad" id="Heater-three" onClick={() => handleAudio("E")}>
-                E
-                <audio 
-                    src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" 
-                    className="clip" 
-                    id="E"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
-            <button className="drum-pad" id="Heater-four" onClick={() => handleAudio("A")}>
-                A
-                <audio 
-                    src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" 
-                    className="clip" 
-                    id="A"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
-            <button className="drum-pad" id="Clap" onClick={() => handleAudio("S")}>
-                S
-                <audio 
-                    src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" 
-                    className="clip" 
-                    id="S"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
-            <button className="drum-pad" id="Open-HH" onClick={() => handleAudio("D")}>
-                D
-                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" 
-                    className="clip" 
-                    id="D"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
-            <button className="drum-pad" id="Kick-n'-Hat" onClick={() => handleAudio("Z")}>
-                Z
-                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" 
-                    className="clip" 
-                    id="Z"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
-            <button className="drum-pad" id="Kick" onClick={() => handleAudio("X")}>
-                X
-                <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" 
-                    className="clip" 
-                    id="X"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
-            <button className="drum-pad" id="Closed-HH" onClick={() => handleAudio("C")}>
-                C
-                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" 
-                    className="clip" 
-                    id="C"
-                >
-                    Your browser does not support html audio tags
-                </audio>
-            </button>
+            <div id="pad-container">
+                <button className="drum-pad" id="Heater-one" onClick={() => handleAudio("Q")}>
+                    Q
+                    <audio
+                        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+                        className="clip"
+                        id="Q"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+                <button className="drum-pad" id="Heater-two" onClick={() => handleAudio("W")}>
+                    W
+                    <audio
+                        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+                        className="clip"
+                        id="W"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+                <button className="drum-pad" id="Heater-three" onClick={() => handleAudio("E")}>
+                    E
+                    <audio
+                        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+                        className="clip"
+                        id="E"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+                <button className="drum-pad" id="Heater-four" onClick={() => handleAudio("A")}>
+                    A
+                    <audio
+                        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+                        className="clip"
+                        id="A"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+                <button className="drum-pad" id="Clap" onClick={() => handleAudio("S")}>
+                    S
+                    <audio
+                        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+                        className="clip"
+                        id="S"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+                <button className="drum-pad" id="Open-HH" onClick={() => handleAudio("D")}>
+                    D
+                    <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+                        className="clip"
+                        id="D"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+                <button className="drum-pad" id="Kick-n'-Hat" onClick={() => handleAudio("Z")}>
+                    Z
+                    <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+                        className="clip"
+                        id="Z"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+                <button className="drum-pad" id="Kick" onClick={() => handleAudio("X")}>
+                    X
+                    <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+                        className="clip"
+                        id="X"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+                <button className="drum-pad" id="Closed-HH" onClick={() => handleAudio("C")}>
+                    C
+                    <audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+                        className="clip"
+                        id="C"
+                    >
+                        Your browser does not support html audio tags
+                    </audio>
+                </button>
+            </div>
         </div>
     )
 }
